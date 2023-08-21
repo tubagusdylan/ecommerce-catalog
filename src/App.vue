0d8ee2de43c4616511059b332d21a171d5d0b85b
tubagusdylan <tubagus.dylanr@gmail.com>
2023-08-21 20:14:25 +0700

    fetch api is OK, styling component in progress

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+  <div :class="[category === 'men\'s clothing' ? 'men_colors' : 'women_colors']" class="container">
+    <ProductDisplay :category="category" :description="description" :image="image" :price="price" :rating="rating" :title="title" />
   </div>
 </template>
 
 <script>
+import ProductDisplay from "./components/ProductDisplay.vue";
+import "./assets/styles/page.css";
+
 export default {
   name: "App",
+  components: {
+    ProductDisplay,
+  },
+  data() {
+    return {
+      category: "",
+      description: "",
+      image: "",
+      price: 0,
+      rating: {},
+      title: "",
+      index: 0,
+      theme: {
+        men_light_blue: "#d6e6ff",
+        women_light_pink: "#FDE2FF",
+      },
+    };
+  },
+  watch: {
+    index: {
+      handler: "getProductData",
+      immediate: true,
+    },
+  },
+  methods: {
+    async getProductData() {
+      try {
+        if (this.index > 19) {
+          this.index = 0;
+        }
+        const response = await fetch(`https://fakestoreapi.com/products/${this.index + 1}`);
+        const product = await response.json();
+        this.category = product.category;
+        this.description = product.description;
+        this.image = product.image;
+        this.price = product.price;
+        this.rating = { ...product.rating };
+        this.title = product.title;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    nextProduct() {
+      this.index = this.index + 1;
+    },
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 15rem;
+}
+.men_colors {
+  background-color: v-bind("theme.men_light_blue");
+  width: 100%;
+  height: 70vh;
+}
+.women_colors {
+  background-color: v-bind("theme.women_light_pink");
+  width: 100%;
+  height: 70vh;
 }
 </style>
--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,9 +1,87 @@
 <template>
-  <div></div>
+  <div class="product-card">
+    <div class="image-product">
+      <img :src="image" alt="gambar" width="80%" />
+    </div>
+    <div class="detail-product">
+      <h1 class="product-title">{{ title }}</h1>
+      <div class="product-category">
+        <span>{{ category }}</span>
+        <span>{{ rating.rate + "/" + 5 }}</span>
+      </div>
+      <hr />
+      <p class="product-desc">
+        {{ description }}
+      </p>
+      <hr />
+      <h2 class="product-price">{{ price }}</h2>
+      <div class="button-wrap">
+        <button class="button-buy">Buy Now</button>
+        <button class="button-next">Next</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "ProductDisplay",
+  props: {
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Object,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      theme: {
+        men_dark_blue: "#002772",
+        men_text: "#3f3f3f",
+        women_dark_pink: "#720060",
+        black: "#1e1e1e",
+        grey: "#dcdcdc",
+        white: "#fff",
+      },
+    };
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.product-card {
+  width: 80%;
+  background-color: v-bind("theme.white");
+  display: flex;
+  padding: 3rem 5rem;
+  border-radius: 5px;
+  box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.1);
+}
+
+.image-product {
+  width: 40%;
+}
+
+.description-product {
+  width: 60%;
+}
+</style>
